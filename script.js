fe43c4bc4a5ec8534d217b75c42a4a394a3bf8c8
Trivedi-800 <02trivedi@gmail.com>
2024-08-27 10:59:47 +0530

    operators

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
-function calculate() {
-    let num1 = parseFloat(document.getElementById("num1").value);
-    let num2 = parseFloat(document.getElementById("num2").value);
-    let operator = document.getElementById("operator").value;
-    let result;
+// function calculate() {
+//     let num1 = parseFloat(document.getElementById("num1").value);
+//     let num2 = parseFloat(document.getElementById("num2").value);
+//     let operator = document.getElementById("operator").value;
+//     let result;
 
-    if (operator === "add") {
-        result = num1 + num2;
-    } else if (operator === "subtract") {
-        result = num1 - num2;
-    } else if (operator === "multiply") {
-        result = num1 * num2;
-    } else if (operator === "divide") {
-        result = num1 / num2;
-    }
+//     if (operator === "add") {
+//         result = num1 + num2;
+//     } else if (operator === "subtract") {
+//         result = num1 - num2;
+//     } else if (operator === "multiply") {
+//         result = num1 * num2;
+//     } else if (operator === "divide") {
+//         result = num1 / num2;
+//     }
 
-    document.getElementById("result").innerText = "Result: " + result;
-}
+//     document.getElementById("result").innerText = "Result: " + result;
+// }
+//&& operator 
+// console.log("hello" &&"Welcome") //------welcome
+// console.log("hello" &&" ")// 
+// console.log("hello" && 15);//15
+// console.log(!!"hnjiii")//true
+//console.log(!!" ")//false
+//<<<<<-----------OR operator--------------->>>>>
+console.log("hello" ||"Welcome")//hello
+console.log("" ||" ")//
+console.log(null || 15);//15
