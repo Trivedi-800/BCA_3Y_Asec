4e258d2dc109c4af81fa99731d16e374d217c4b7
Trivedi-800 <02trivedi@gmail.com>
2024-09-03 11:53:54 +0530

    new work

---
+++ script 3.js
@@ -0,0 +1,65 @@
+// let str ="I am currently pursuing BCA from GLA University."
+// console.log(str.length)
+//var a =12345
+//var b =a.toString();
+//console.log(typeof(a))
+//console.log(typeof(b))
+// console.log(str.indexOf())
+
+//-----------------------------slice
+// let str ="I am currently pursuing BCA from GLA University."
+// console.log(str.slice(10))
+// console.log(str.slice(1,8))
+// console.log(str.slice(-1))
+// console.log(str.slice(15,12)) //greater value before 
+// console.log(str.slice(-6,-4))
+// console.log(str.slice(-6,4))//ulta nahi chlega 
+// console.log(str.slice(6,-4))
+
+//---------------------------substring
+// let str ="I am currently pursuing BCA from GLA University."
+// console.log(str.substring(9))
+// console.log(str.substring(3,6))
+// console.log(str.substring(-9))
+// console.log(str.substring(15,12))
+// console.log(str.substring(-6,-4))
+// console.log(str.substring(-6,3))
+// console.log(str.substring(6,-3))
+
+//-------------------....>>>>>>>>>>>>>>>>
+//let str ="I am currently pursuing BCA from GLA University."
+// console.log(str.toUpperCase())
+// console.log(str.includes("Currently"))
+// console.log(str.includes("currently"))
+
+//-----------------&&&&search match
+//let str ="GLA University is located in Mathura ";
+// console.log(str.search("is"))
+// console.log(str.search("z"))
+// console.log(str.match("in"))
+// console.log(str.match(/d/g))
+// console.log(str.match(/e/g))
+// let str ="GLA University is located in Mathura ";
+// console.log(str.replace('is','was'))
+// console.log(str.replace(/is/g,'was'))
+
+//-------------->
+// let str ="GLA University is located in Mathura ";
+// console.log(str.endsWith("ra "))
+// console.log(str.startsWith("GLA "))
+
+//--------------------->concatenate
+// let str1="hello";
+// let str2="everyone";
+// let str3="thank you";
+// console.log(str1.concat(str2,str3))
+//console.log(str.)
+
+// let str ="     jadu hai mer a naam "
+// console.log(str.trim())
+
+// let str ="GLA University is located in Mathura ";
+// console.log(str.charAt(20));let str ="GLA University is located in Mathura ";
+
+// console.log(str.charCodeAt(0));
+// console.log(String.fromCharCode(101))
