f3e6500d4541afba6776b90a4b275955c0769f18
Trivedi-800 <02trivedi@gmail.com>
2024-09-06 10:15:20 +0530

    prctc

--- script2.js
+++ script2.js
@@ -1,7 +1,6 @@
-window.alert("show doc")
-let ip=window.prompt("enter name ")
-
-
-
-document.getElementById("demo").innerHTML=`<h1>Hello,${ip}</h1>`
-document.write("Thankyou")
+// window.alert("show doc")
+// let ip=window.prompt("enter name ")
+// document.getElementById("demo").innerHTML=`<h1>Hello,${ip}</h1>`
+// document.write("Thankyou")
+function show(ip){
+document.getElementById("demo").innerText=`Hello,${ip}`}
